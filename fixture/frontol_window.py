95b245e338ace41315b10676eb49c89a5342a05e
Александр <leg020@inbox.ru>
2020-07-02 16:35:28 +0300

    Обновлена регистрация

--- fixture/frontol_window.py
+++ fixture/frontol_window.py
@@ -22,8 +22,41 @@ class FrontolWindow:
         else:
             return -1
 
-    def registration(self):
+    def close_main_window(self):
+        self.app.click_element(window_name='Супервизор', button_name='Выход в ОС')
+        self.app.click_button('~')
+
+    def enter_position(self, place_position, count):
+        i = 1
         self.app.click_element(window_name='Супервизор', button_name='Регистрация...')
         self.app.click_button('{F6}')
-        self.app.click_button('{DOWN}')
+        while i < place_position:
+            self.app.click_button('{DOWN}')
+            i = i + 1
         self.app.click_button('~')
+        i = 1
+        while i < count:
+            self.app.click_button('{UP}')
+            i = i + 1
+        self.app.click_button('~')
+        self.app.click_button('{ESC}')
+
+    def close_check(self, type_pay: int):
+        i = 1
+        self.app.click_button('~')
+        self.app.click_button('~')
+        while i < type_pay:
+            self.app.click_button('{DOWN}')
+            i = i + 1
+        self.app.click_button('~')
+        self.app.click_button('^ {F2}')
+        time.sleep(2)
+
+    def exit(self):
+        self.app.click_button('{ESC}')
+        self.app.click_button('~')
+
+    def registration(self, place_position, count):
+        self.enter_position(place_position, count)
+        self.close_check(2)
+        self.exit()
--- test/registration.py
+++ test/registration.py
@@ -2,5 +2,6 @@ from fixture.frontol_window import FrontolWindow
 
 
 app = FrontolWindow(target='C:\\Program Files (x86)\\ATOL\\Frontol6\\BIN\\Frontol.exe')
-app.open_main_window(have_cassa=False)
-app.registration()
+app.open_main_window(have_cassa=True)
+app.registration(3, 2)
+app.close_main_window()
