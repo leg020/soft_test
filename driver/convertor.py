75ad34c5c31c79543ee11502e7abe29c1ac0abed
Александр <leg020@inbox.ru>
2020-07-14 18:35:46 +0300

    Написал класс конвертор для проверки присланных данных и их перевода в необходимый формат так же файл main содержит 2 класса 1 - для работы по старому 2 - для работы через api и с файлом

---
+++ driver/convertor.py
@@ -0,0 +1,58 @@
+from model.settings import Settings
+from model.data import Data
+from model.positions_in_check import *
+
+class Convertor:
+
+    def __init__(self):
+        self.err = []
+        self.data = None
+
+    def convert_positions_to_test_model(self, positions_list:[]):
+        data = []
+        try:
+            for row in positions_list:
+                data.append(PositionsInCheck(place_in_list=row['place_in_list'],
+                                             cout=row['cout'],
+                                             need_mark=row['need_mark'],
+                                             mark=row['mark']))
+            return data
+        except:
+            self.err.append('There is exception in positions')
+            return -1
+
+    def convert_documents_to_test_model(self, file):
+        data = []
+        for row in file['data']:
+            positions = self.convert_positions_to_test_model(row['positions'])
+            if positions != -1:
+                try:
+                    data.append(Checks(check_number=row['check_number'],
+                                           document_type=row['document_type'],
+                                           report_type=row['report_type'],
+                                           check_type=row['check_type'],
+                                           help_setting=row['help_setting'],
+                                           type_close=row['type_close'],
+                                           sale=row['sale'], positions=positions))
+                except:
+                    self.err.append('There is exception in documents')
+                    return -1
+        return data
+
+
+
+    def convert_to_test_model(self, file):
+        data = self.convert_documents_to_test_model(file=file)
+        if data != -1:
+            try:
+                self.data = Data(settings=Settings(target=file['settings']['target'],
+                                                   scaner_port=file['settings']['scaner_port'],
+                                                   scaner_boundrate=file['settings']['scaner_boundrate'],
+                                                   have_cassa=file['settings']['have_cassa']),
+                                 data=data)
+                return 0
+            except:
+                self.err.append('There is exception')
+                return -1
+        else:
+            return -1
--- soft_testing.py
+++ soft_testing.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 import json
-from fixture.main import Main
+from fixture.main import Maining
+from driver.convertor import Convertor
 
 
 server = Flask(__name__)
@@ -8,12 +9,17 @@ server = Flask(__name__)
 
 @server.route('/add_task', methods=['GET', 'POST'])
 def read_json_file():
+    convertor = Convertor()
     file = request.get_json(silent=True)
-    main = Main(file)
-    main.start_frontol()
-    main.make_document()
-    main.exit_frontol()
-    return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
+    flag = convertor.convert_to_test_model(file)
+    if flag == 0:
+        main = Maining(convertor.data)
+        main.start_frontol()
+        main.make_document()
+        main.exit_frontol()
+        return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
+    if flag == -1:
+        return json.dumps({'success': False}), 200, {'ContentType': 'application/json'}
 
 if __name__ == '__main__':
     server.run(host="127.0.0.1", port="8000", debug=True)
